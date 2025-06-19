1e133cbfd98bdeedb70ec569a29b84f2e2c1c1be
philipgisore <philipgisore7@gmail.com>
2025-06-19 09:58:18 +0300

    inheritance_deepdive

--- inheritance.py
+++ inheritance.py
@@ -20,4 +20,36 @@ d = Dog()
 d.eat()
 d.sleep()
 d.bark()
-    
+  
+#Multiple inheritance, a child class inherits from multiple parent classes
+class Flyer:
+    def fly(self):
+        print("Flying...")
+        
+class Swimmer:
+    def swim(self):
+        print("Swimming...")
+        
+class Duck(Flyer, Swimmer):
+    pass
+
+duck = Duck()
+duck.fly()
+duck.swim()
+
+#Multilevel inheritance, a child class inherits from a parent class which inherits from another parent class
+class Vehicle:
+    def move(self):
+        print("Moving...")
+        
+class Car(Vehicle):
+    pass
+
+class ElectricCar(Car):
+    def charge(self):
+        print("Charging...")
+        
+tesla = ElectricCar()
+tesla.move()
+tesla.charge()
+  
